0ec2c4d120571528d957e0dd1e470cf5c873a704
aibek <4330131+heyaibek@users.noreply.github.com>
2024-02-06 13:38:42 +0100

    solve 120

---
+++ 120-triangle.swift
@@ -0,0 +1,23 @@
+class Solution {
+  func minimumTotal(_ triangle: [[Int]]) -> Int {
+    var arr = triangle
+	
+    for i in stride(from: arr.count - 1, through: 0, by: -1) {
+      for j in stride(from: arr[i].count - 1, through: 0, by: -1) {
+        let curr = arr[i][j]
+        
+        var prevLeft = 0
+        var prevRight = 0
+
+        if i < arr.count - 1 {
+          prevLeft = arr[i + 1][j]
+          prevRight = arr[i + 1][j + 1]
+        }
+        
+        arr[i][j] = min(curr + prevLeft, curr + prevRight)
+      }
+    }
+    
+    return arr[0][0]
+  }
+}
