229402a4605bcc6b9e7cc2adbe697d02baa26061
aibek <4330131+heyaibek@users.noreply.github.com>
2024-02-07 15:38:34 +0100

    add big-o estimations for 217

--- 217-contains-duplicate.swift
+++ 217-contains-duplicate.swift
@@ -1,3 +1,10 @@
+//
+// Time complexity: O(N)
+// Space complexity: O(N)
+//
+//   where N is the number of elements in the nums array
+//
+
 class Solution {
   func containsDuplicate(_ nums: [Int]) -> Bool {
     nums.count != Set(nums).count
